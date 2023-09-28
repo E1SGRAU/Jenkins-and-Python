93bfe6995ca5b13b3274bd180a03a0047db927cc
Alex13 <105718520+E1SGRAU@users.noreply.github.com>
2023-09-28 12:13:11 +0300

    Update script_pubinfo.py

--- Instance/script_pubinfo.py
+++ Instance/script_pubinfo.py
@@ -1,3 +1,9 @@
 from lib.currency import get_currency
+
+# Публічні дані по валютам:
+# -> https://api.privatbank.ua/#p24/exchange <- 
+# з посилання можна отримати URL
 URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+
+#Підключаємо функцію
 get_currency(URL)
