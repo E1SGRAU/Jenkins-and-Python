dd5cff6bb0573ebbb95798fee03b5222611ad54c
Alex13 <105718520+E1SGRAU@users.noreply.github.com>
2023-09-28 12:54:15 +0300

    Update upload_file.py

--- S3-bucket-scripts/upload_file.py
+++ S3-bucket-scripts/upload_file.py
@@ -9,7 +9,7 @@ s3_bucket_name = "alex13"
 s3_client = boto3.client("s3", region_name=aws_region)
 
 #Шлях до файлу, або папки що треба загрузити на S3 bucket
-local_file_path = r"d:\Download\currency.txt'"
+local_file_path = r"d:\Download\currency.txt"
 
 def upload_files(file_name, bucket, object_name=None, args=None):
     if object_name is None:
